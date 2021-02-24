c9a8e1e24c42ccb05db96c4297a36019932d1f0a
Kumar <859441@cognizant.com>
2021-02-24 15:50:51 +0530

    msg

--- src/main/java/com/example/jdbcex/JdbcexampleApplication.java
+++ src/main/java/com/example/jdbcex/JdbcexampleApplication.java
@@ -3,9 +3,11 @@ package com.example.jdbcex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@Component
 public class JdbcexampleApplication {
 
 	public static void main(String[] args) {
