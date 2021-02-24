d0cf5582e7e726c1e402b33531d751211f7330ba
859441 <859441@LTIN201284.cts.com>
2021-02-24 13:36:51 +0530

    Add Project

--- src/main/java/com/example/jdbcex/model/Book.java
+++ src/main/java/com/example/jdbcex/model/Book.java
@@ -1,7 +1,13 @@
 package com.example.jdbcex.model;
 
-public class Book {
+import java.io.Serializable;
+
+public class Book implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	int id;
 	String author;
 	String desc;
